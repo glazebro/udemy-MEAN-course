0fa3a550d0a3bfa802fdcc1b42e295c4332a983e
MIke Glazebrook <mikefolio@gmail.com>
2020-07-16 06:48:21 -0600

    daily progress
    updating repo and continued course work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+//import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule
+   //MatButtonModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
