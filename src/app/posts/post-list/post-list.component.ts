7355877ec5281fbd730895c2ed340b7ed73b587e
MIke Glazebrook <mikefolio@gmail.com>
2020-07-17 07:54:54 -0600

    getting posts and cleaning up code
    getting posts and cleaning up code

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 
 import { Post } from "../post.model";
+import { PostsService } from "../posts.service";
 
 @Component({
     selector: 'app-post-list',
@@ -8,11 +10,25 @@ import { Post } from "../post.model";
     styleUrls: ['./post-list.component.css']
 })
 
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
     // posts=[ 
     //     {title:'First Post', content: 'This is the first post\'s conent'},
     //     {title:'Second Post', content: 'This is the second post\'s conent'},
     //     {title:'Third Post', content: 'This is the third post\'s conent'}
     // ];
-    @Input() posts: Post[]=[];
+    posts: Post[]=[];
+    private postsSub: Subscription;
+
+    constructor(public postsService: PostsService) {}
+    ngOnInit(){
+        this.posts = this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostUpdateListener()
+            .subscribe((posts: Post[]) => {
+                this.posts = posts;
+            });
+    }
+
+    ngOnDestroy() {
+        this.postsSub.unsubscribe();
+    }
 }
