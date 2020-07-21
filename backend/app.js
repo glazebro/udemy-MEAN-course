ad8414baa80e000adf4036d203c3205a0ae5713f
MIke Glazebrook <mikefolio@gmail.com>
2020-07-21 08:45:48 -0600

    connected mongodb
    created and connected mongodb with the use of mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,54 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'
-    );
-    res.setHeader('Access-Control-Allow-Headers', 
-    'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 
-    'GET, POST, PATCH, DELETE, OPTIONS'
-    );
-    next();
+mongoose.connect("mongodb+srv://mike:0etzWSeA80dhS1e8@cluster0.l6roy.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() => {
+    console.log('Connected to database!');
+})
+.catch(() => {
+    console.log('Connection failed!');
 });
 
-app.use('/api/posts',(req, res, next) => {
-    const posts =[
-        {
-            id: 'fadf124211', 
-            title:'First server-side post', 
-            content:'This is coming from the server'
-        },
-        {
-            id: 'kdfa23875', 
-            title:'Second server-side post', 
-            content:'This is coming from the server'
-        }
-    ];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+      title: req.body.title,
+      content: req.body.content
+  });
+  post.save();
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
+  Post.find()
+  .then(documents => {
     res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: posts
+        message: "Posts fetched successfully!",
+        posts: documents
     });
+  });
 });
 
 module.exports = app;
