39b9ef348e5e0c93a357a1681fd9cbea57760fe2
glazebro <mikefolio@gmail.com>
2020-08-20 10:22:42 -0500

    error fixing and loading on new machine

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,21 +1,27 @@
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Injectable } from '@angular/core';
-
-import { AuthService } from "./auth.service";
-
-@Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService, private router: Router) { }
-
+import {
+    CanActivate,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    Router
+  } from "@angular/router";
+  import { Injectable } from "@angular/core";
+  import { Observable } from "rxjs";
+  
+  import { AuthService } from "./auth.service";
+  
+  @Injectable()
+  export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) {}
+  
     canActivate(
-        route: ActivatedRouteSnapshot, 
-        state: RouterStateSnapshot
-        ): boolean | Observable<boolean> | Promise<boolean> {
-            const isAuth = this.authService.getIsAuth();
-            if(isAuth){
-                this.router.navigate(['/']);
-            } 
-            return isAuth;
+      route: ActivatedRouteSnapshot,
+      state: RouterStateSnapshot
+    ): boolean | Observable<boolean> | Promise<boolean> {
+      const isAuth = this.authService.getIsAuth();
+      if (!isAuth) {
+        this.router.navigate(['/login']);
+      }
+      return isAuth;
     }
-}
+  }
+  
